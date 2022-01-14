55a5f33c26c692fbd849db3cb392b349a8884e78
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-01-14 13:13:20 +0100

    test: improve working features

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # hivemq-client-examples
+
+Commands to run the samples
+
+```shell
+brew install kafka
+
+docker-compose up -d
+
+kafka-console-producer --broker-list localhost:19092 --topic test
+
+kafka-console-consumer --bootstrap-server localhost:19092 --topic test --from-beginning
+
+kafka-topics --bootstrap-server localhost:19092 --topic test --describe
+```
--- src/test/java/KafkaClientTest.java
+++ src/test/java/KafkaClientTest.java
@@ -1,7 +1,11 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.config.TopicConfig;
+import org.apache.kafka.common.errors.RecordTooLargeException;
 import org.apache.kafka.common.errors.TopicExistsException;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
@@ -10,29 +14,62 @@ import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KafkaClientTest {
-    final String senderTopic = "test_sender";
+    final String senderTopic = "test";
     private static final Logger log = LoggerFactory.getLogger(KafkaClientTest.class);
     private final Properties properties;
+
     KafkaClientTest() throws UnknownHostException {
         properties = getProperties();
     }
-    @Test
-    public void sendMessage() {
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        final ProducerRecord<String, String> record = new ProducerRecord<>(senderTopic, "key", "value");
-        producer.send(record, (m, e) -> {
-            if (e != null){
-                log.debug("Send failed for record {}", record, e);}
-            else {
+    @Test
+    public void sendByteMessage() throws ExecutionException, InterruptedException {
+        SendMessageOfSize(512);
+    }
+
+    @Test
+    public void sendLargeByteMessage() throws ExecutionException, InterruptedException {
+        SendMessageOfSize(2048 * 1024);
+    }
+
+    private void SendMessageOfSize(int size) throws InterruptedException, ExecutionException {
+        KafkaProducer<String, byte[]> producer = new KafkaProducer<>(properties);
+        byte[] payload = RandomStringUtils.randomAscii(size).getBytes(StandardCharsets.UTF_8);
+        final ProducerRecord<String, byte[]> record = new ProducerRecord<>(senderTopic, "key", payload);
+        AtomicBoolean sent = new AtomicBoolean(false);
+        Future<RecordMetadata> future = producer.send(record, (m, e) -> {
+            if (e != null) {
+                log.debug("Send failed for record {}", record, e);
+            } else {
                 System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", m.topic(), m.partition(), m.offset());
+                sent.set(true);
             }
         });
+        try {
+            future.get();
+        } catch (java.util.concurrent.ExecutionException e) {
+            if(e.getCause() instanceof org.apache.kafka.common.errors.RecordTooLargeException){
+                RecordTooLargeException recordInfo = ((RecordTooLargeException) e.getCause());
+                String message = recordInfo.getMessage();
 
+            }else{
+                log.error(e.getCause().getClass().getCanonicalName());
+            }
+
+        }catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        assertTrue(future.isDone());
+        assertTrue(sent.get());
     }
 
     @NotNull
@@ -40,9 +77,9 @@ public class KafkaClientTest {
         Properties config = new Properties();
         config.put("client.id", InetAddress.getLocalHost().getHostName());
         config.put("bootstrap.servers", "localhost:19092");
-        config.put(ProducerConfig.ACKS_CONFIG, "all");
+        //config.put(ProducerConfig.ACKS_CONFIG, "all");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonSerializer");
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
         return config;
     }
 
@@ -58,20 +95,22 @@ public class KafkaClientTest {
             }
         }
     }
+
     @Test
     public void alterTopic() throws ExecutionException, InterruptedException {
 
         final AdminClient adminClient = AdminClient.create(properties);
 
-        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC,senderTopic);
-        ConfigEntry entry = new ConfigEntry(TopicConfig.MAX_MESSAGE_BYTES_CONFIG, String.valueOf(2048*2048));
-        AlterConfigOp op = new AlterConfigOp(entry,AlterConfigOp.OpType.SET);
+        ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, senderTopic);
+        ConfigEntry entry = new ConfigEntry(TopicConfig.MAX_MESSAGE_BYTES_CONFIG, String.valueOf(2048 * 2048));
+        AlterConfigOp op = new AlterConfigOp(entry, AlterConfigOp.OpType.SET);
 
-        Map<ConfigResource,Collection<AlterConfigOp>> configs = new HashMap<>(1);
+        Map<ConfigResource, Collection<AlterConfigOp>> configs = new HashMap<>(1);
         configs.put(configResource, List.of(op));
-        adminClient.incrementalAlterConfigs(configs).all().get();
+        KafkaFuture<Void> alterFuture = adminClient.incrementalAlterConfigs(configs).all();
         //final NewTopic newTopic = new NewTopic(senderTopic, Optional.empty(), Optional.empty());
-
+        alterFuture.get();
+        assertTrue(alterFuture.isDone());
 
     }
 }
