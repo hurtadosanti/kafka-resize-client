aa6b89c8b650a7c20f109bb268cfe76042b02dc6
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-01-14 16:52:29 +0100

    feat: receive messages

--- src/test/java/KafkaClientTest.java
+++ src/test/java/KafkaClientTest.java
@@ -1,5 +1,8 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.config.ConfigResource;
@@ -14,10 +17,12 @@ import org.slf4j.LoggerFactory;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -40,33 +45,53 @@ public class KafkaClientTest {
         sendMessageOfSize(2048 * 1024);
     }
 
+    @Test
+    public void receiveMessage() {
+        KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(senderTopic));
+        AtomicInteger counter = new AtomicInteger();
+        while (counter.get() <10) {
+            ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofMillis(100));
+
+            if (records.count() > 0) {
+                records.forEach(record -> {
+                    counter.getAndIncrement();
+                    String s = new String(record.value(), StandardCharsets.UTF_8);
+                    System.out.println(record.key()+"-"+ record.value().length);
+                });
+            }
+        }
+        //consumer.close();
+    }
+
     private void sendMessageOfSize(int size) throws ExecutionException, InterruptedException {
         KafkaProducer<String, byte[]> producer = new KafkaProducer<>(properties);
-        byte[] payload = RandomStringUtils.randomAscii(size).getBytes(StandardCharsets.UTF_8);
+        byte[] payload = ("start - "+RandomStringUtils.randomAscii(size)+" - end").getBytes(StandardCharsets.UTF_8);
         AtomicBoolean sent = new AtomicBoolean(false);
         Future<RecordMetadata> future = null;
         try {
-            future = sendMessage(producer,payload,sent);
+            future = sendMessage(producer, payload, sent);
         } catch (java.util.concurrent.ExecutionException e) {
-            if(e.getCause() instanceof org.apache.kafka.common.errors.RecordTooLargeException){
+            if (e.getCause() instanceof org.apache.kafka.common.errors.RecordTooLargeException) {
                 RecordTooLargeException recordInfo = ((RecordTooLargeException) e.getCause());
                 String message = recordInfo.getMessage();
                 producer.close();
-                properties.setProperty(ProducerConfig.MAX_REQUEST_SIZE_CONFIG,String.valueOf(payload.length+256));
+                properties.setProperty(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, String.valueOf(payload.length + 256));
                 producer = new KafkaProducer<>(properties);
                 alterTopic();
-                future = sendMessage(producer,payload,sent);
-            }else{
+                future = sendMessage(producer, payload, sent);
+            } else {
                 log.error(e.getCause().getClass().getCanonicalName());
             }
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
         assert future != null;
         assertTrue(future.isDone());
         assertTrue(sent.get());
     }
+
     private Future<RecordMetadata> sendMessage(KafkaProducer<String, byte[]> producer, byte[] payload, AtomicBoolean sent) throws ExecutionException, InterruptedException {
         final ProducerRecord<String, byte[]> record = new ProducerRecord<>(senderTopic, "key", payload);
         Future<RecordMetadata> future = producer.send(record, (m, e) -> {
@@ -87,8 +112,12 @@ public class KafkaClientTest {
         config.put("client.id", InetAddress.getLocalHost().getHostName());
         config.put("bootstrap.servers", "localhost:19092");
         //config.put(ProducerConfig.ACKS_CONFIG, "all");
+
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "01");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArrayDeserializer");
         return config;
     }
 
